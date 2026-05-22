1590f4a89235d880af103d29a492cab78600ca57
Sofian <messaoui.sofian@hotmail.fr>
2026-05-22 08:23:41 +0000

    FEAT : the header file for the sandpile function

---
+++ sandpiles/sandpiles.h
@@ -0,0 +1,11 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+/**
+ * sandpiles_sum - Compute the sum of two sandpiles
+ * @grid1: First 3x3 grid (modified in place, stable when done)
+ * @grid2: Second 3x3 grid (unchanged)
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+#endif /* SANDPILES_H */
