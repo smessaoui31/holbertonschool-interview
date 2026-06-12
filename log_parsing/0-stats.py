dcf7a0afd1c75651045c5abc9dc23bbc87801fe1
Sofian <messaoui.sofian@hotmail.fr>
2026-06-12 08:07:55 +0000

    FIX: Correctly handle line endings in log parsing

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
 
     try:
         for line in sys.stdin:
-            m = PATTERN.match(line.rstrip('\n'))
+            m = PATTERN.match(line.rstrip('\r\n'))
             if m:
                 status = int(m.group(1))
                 total_size += int(m.group(2))
